e2341e51925895ffb829ebb6e75d9a341d02ac97
Affandi1807 <125229924+Affandi1807@users.noreply.github.com>
2023-11-09 11:18:09 +0700

    nav

--- app/src/main/java/com/example/adminmobile/MainActivity.java
+++ app/src/main/java/com/example/adminmobile/MainActivity.java
@@ -2,13 +2,65 @@ package com.example.adminmobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.adminmobile.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         setContentView(R.layout.activity_main);
+
+        frameLayout = findViewById(R.id.konten);
+
+        MainActivity mainActivity = new MainActivity();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.konten, new home());
+        transaction.commit();
+
+        BottomNavigationView bottomNavigationView;
+        bottomNavigationView = findViewById(R.id.bottom_NavView);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.menu1) {
+                Log.d("menu1", "onNavigationItemSelected: yes");
+                FragmentManager fragmentManager1 = getSupportFragmentManager();
+                FragmentTransaction transaction1 = fragmentManager1.beginTransaction();
+                transaction1.replace(R.id.konten, new home());
+                transaction1.commit();
+                return true;
+            } else if (item.getItemId() == R.id.menu2) {
+                MainActivity mainActivity1 = new MainActivity();
+                FragmentManager fragmentManager1 = getSupportFragmentManager();
+                FragmentTransaction transaction1 = fragmentManager1.beginTransaction();
+                transaction1.replace(R.id.konten, new notifikasi());
+                transaction1.commit();
+            } else if (item.getItemId() == R.id.menu3) {
+                FragmentManager fragmentManager1 = getSupportFragmentManager();
+                FragmentTransaction transaction1 = fragmentManager1.beginTransaction();
+                transaction1.replace(R.id.konten, new akunprofile());
+                transaction1.commit();
+            }
+            return true;
+        });
     }
 }
--- app/src/main/java/com/example/adminmobile/tambahDataMobil.java
+++ app/src/main/java/com/example/adminmobile/tambahDataMobil.java
@@ -1,14 +1,61 @@
 package com.example.adminmobile;
-
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.ImageView;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class tambahDataMobil extends AppCompatActivity {
 
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private ImageView imageView;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah_data_mobil);
+
+        imageView = findViewById(R.id.addimage);
+        Button btn = findViewById(R.id.simpan);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectImage();
+            }
+        });
+    }
+
+    private void selectImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Pilih Gambar"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri imageUri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                imageView.setImageBitmap(bitmap);
+                Toast.makeText(this, "Gambar dipilih!", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Terjadi kesalahan dalam memuat gambar.", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
